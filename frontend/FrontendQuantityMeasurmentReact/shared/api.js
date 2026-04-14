1439a1fba81e3c624b0e000f490750e3c64bcb6e
Suyash sharma <suyashsharma942@gmail.com>
2026-04-14 16:43:19 +0530

    Delete frontend/FrontendQuantityMeasurmentReact directory

--- frontend/FrontendQuantityMeasurmentReact/shared/api.js
+++
@@ -1,7 +0,0 @@
-/**
- * Shared code between client and server
- * Useful to share small pure JS functions that can be used on both client and server
- */
-
-// Example:
-// export const formatMessage = (msg) => `Formatted: ${msg}`;
